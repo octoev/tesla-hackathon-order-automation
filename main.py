7405e8c559eed0e55e47d6186a6057736421ee6e
Nish C <nishanthchandradas@gmail.com>
2024-05-10 15:31:11 +0100

    poc

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
-from subprocess import PIPE, run
 import time
+from subprocess import PIPE, run
 
 from playwright.sync_api import sync_playwright
 
@@ -19,11 +19,19 @@ def get_username_password_otp():
     return {"username": username, "password": password, "otp": otp}
 
 
-def order_car():
+def order_car(
+    model,
+    spec,
+    colour,
+    interior,
+    wheels,
+    tow_hitch=False,
+    enhanced=False,
+    self_driving=False,
+):
     credentials = get_username_password_otp()
-    print(credentials)
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False, slow_mo=500)
+        browser = p.chromium.launch(headless=False, slow_mo=200)
         context = browser.new_context()
         page = context.new_page()
         page.goto("https://www.tesla.com/teslaaccount/business/orders/")
@@ -47,17 +55,78 @@ def order_car():
         page.get_by_label("passcode").fill(credentials["otp"])
         page.get_by_role("button", name="Submit").click()
         page.get_by_label("Select an Account").click()
-        page.get_by_label("", exact=True).get_by_text("Octopus Electric Vehicles").click()
+        page.get_by_label("", exact=True).get_by_text(
+            "Octopus Electric Vehicles"
+        ).click()
         page.get_by_role("button", name="Continue").click()
         page.get_by_role("button", name="Create Order").click()
+        if model == "Model 3":
+            page.get_by_text("Model 3").click()
+        elif model == "Model Y":
+            page.get_by_text("Model Y").click()
+        page.get_by_role("button", name="Accept").click()
         page.get_by_role("button", name="Next").click()
-        page.get_by_text("Performance All-Wheel Drive£59,990328 mi Range (WLTP)*2.9 sec 0-60 mph163 mph").click()
-        page.get_by_role("img", name="Ultra Red").locator("image").click()
-        page.get_by_role("img", name="Black and White").locator("image").click()
-        page.get_by_role("button", name="Add to Order").click()
+        page.mouse.wheel(0, 250)
+        if spec == "rear wheel drive":
+            page.get_by_text("Rear-Wheel Drive", exact=True).click()
+        elif spec == "long range":
+            page.get_by_text("Long Range All-Wheel Drive").click()
+        elif spec == "performance":
+            page.get_by_text("Performance All-Wheel Drive").click()
+        page.mouse.wheel(0, 250)
+        if colour == "Pearl White Multi-Coat":
+            page.get_by_role("img", name="Pearl White Multi-Coat").locator(
+                "image"
+            ).click()
+        elif colour == "Deep Blue Metallic":
+            page.get_by_role("img", name="Deep Blue Metallic").locator("image").click()
+        elif colour == "Solid Black":
+            page.get_by_role("img", name="Solid Black").locator("image").click()
+        elif colour == "Ultra Red":
+            page.get_by_role("img", name="Ultra Red").locator("image").click()
+        elif colour == "Stealth Grey":
+            page.get_by_role("img", name="Stealth Grey").locator("image").click()
+        elif colour == "Quicksilver":
+            page.get_by_role("img", name="Quicksilver").locator("image").click()
+        page.mouse.wheel(0, 500)
+
+        if wheels == '20" Induction alloy wheels':
+            page.get_by_role("img", name="’’ Induction Wheels").locator("image").click()
+        elif wheels == '19" Gemini alloy wheels':
+            page.get_by_role("img", name="’’ Gemini Wheels").locator("image").click()
+        elif wheels == '18" Photon alloy wheels':
+            page.get_by_role("img", name="’’ Photon Wheels").locator("image").click()
+        elif wheels == '19" Nova alloy wheels':
+            page.get_by_role("img", name="’’ Nova Wheels").locator("image").click()
+
+        if tow_hitch:
+            page.get_by_label("Select Option - Tow Hitch - £").click()
+
+        if enhanced:
+            page.get_by_label("Select Option - Enhanced").click()
+        elif self_driving:
+            page.get_by_label("Select Option - Full Self-").click()
+
+        if interior == "black":
+            page.get_by_role("img", name="Black", exact=True).locator("image").click()
+        elif interior == "black and white":
+            page.get_by_role("img", name="Black and White").locator("image").click()
+        elif interior == "All Black":
+            page.get_by_role("img", name="All Black").locator("image").click()
+        page.mouse.wheel(0, 2000)
+        page.get_by_role("button", name="Add to Order").click(timeout=10000000)
         time.sleep(60000)
         context.close()
         browser.close()
 
 
-order_car()
+order_car(
+    model="Model 3",
+    colour="Stealth Grey",
+    interior="black",
+    spec="rear wheel drive",
+    wheels='19" Nova alloy wheels',
+    tow_hitch=True,
+    enhanced=False,
+    self_driving=True,
+)
