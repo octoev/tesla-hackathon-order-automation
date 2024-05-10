b279a8b51926a4c2d61f6d612d5fd5ce790ebf9c
Nish C <nishanthchandradas@gmail.com>
2024-05-10 12:09:56 +0100

    auth

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
+from subprocess import PIPE, run
+
 from playwright.sync_api import sync_playwright
-from subprocess import run, PIPE
 
 
 def get_username_password_otp():
@@ -12,14 +13,14 @@ def get_username_password_otp():
     blob = json.loads(res.stdout.decode())
     username = blob["fields"][0]["value"]
     password = blob["fields"][1]["value"]
-    otp = blob["fields"][3]["value"]
+    otp = blob["fields"][3]["totp"]
 
     return {"username": username, "password": password, "otp": otp}
 
 
-
 def order_car():
     credentials = get_username_password_otp()
+    print(credentials)
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False, slow_mo=500)
         context = browser.new_context()
@@ -28,14 +29,24 @@ def order_car():
         page.get_by_label("Email", exact=True).click()
         page.get_by_label("Email", exact=True).fill(credentials["username"])
         page.get_by_role("button", name="Next").click()
+        print("Username filled in")
         page.frame_locator("#sec-cpt-if").locator("#robot-checkbox").check()
         page.frame_locator("#sec-cpt-if").get_by_text("Proceed").click()
         page.get_by_label("password").click()
         page.get_by_label("password").fill(credentials["password"])
-        page.get_by_role("button", name="Sign In").click()
+        print("Password filled in")
 
-        # ---------------------
-        context.close()
-        browser.close()
+        page.get_by_role("button", name="Sign In").click()
+        print("Sign in clicked")
+        passcode_input = page.locator('[name="passcode"]')
+        passcode_input.wait_for(timeout=0)
+        credentials = get_username_password_otp()
+        page.get_by_label("passcode").fill(credentials["otp"], timeout=0)
+        page.get_by_role("button", name="Submit").click()
+        fake_input = page.locator('[name="fake"]')
+        fake_input.wait_for(timeout=0)
+        # context.close()
+        # browser.close()
+
 
 order_car()
