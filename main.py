fe2463ca0d4a8904999eb2ac0cca617dc044d3e4
Nish C <nishanthchandradas@gmail.com>
2024-05-10 10:54:32 +0100

    refactor username pasword function

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import json
-from subprocess import run, PIPE
+from subprocess import PIPE, run
 
 
-def get_username_password():
-    res = run(["op", 
-               "item", 
-               "get", 
-               "Tesla Business Portal",  
-               "--format", 
-               "json"], 
-                stdout=PIPE, stderr=PIPE
-            )
+def get_username_password_otp():
+    res = run(
+        ["op", "item", "get", "Tesla Business Portal", "--format", "json"],
+        stdout=PIPE,
+        stderr=PIPE,
+    )
     blob = json.loads(res.stdout.decode())
     username = blob["fields"][0]["value"]
     password = blob["fields"][1]["value"]
     otp = blob["fields"][3]["value"]
-    print(username, password, otp)
 
-get_username_password()
+    return {"username": username, "password": password, "otp": otp}
+
+
+get_username_password_otp()
